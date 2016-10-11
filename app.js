4d289bd5ab3abcdcaa423b057f22cbb67a9ae555
Luis Diaz <preston-91@hotmail.com>
2016-10-11 00:03:48 +0000

    update

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.get('/', function(req, res,next) {
 var coin;
 var coins;
 
-
 request({
     url:'https://api.cryptonator.com/api/ticker/btc-usd',
     json:true},
@@ -27,8 +26,7 @@ request({
         
         if(!error && response.statusCode === 200){
             coin = body;
-            console.log(body);
-            console.log(coin);
+            //console.log(body);
           conn.collection('crypto').insert(coin);
          
          conn.collection('crypto').find().toArray(function(err,results){
@@ -52,8 +50,8 @@ request({
         
         if(!error && response.statusCode === 200){
             coin = body;
-            console.log(body);
-            console.log(coin);
+            //console.log(body);
+            //console.log(coin);
           conn.collection('crypto').insert(coin);
          
          conn.collection('crypto').find().toArray(function(err,results){
@@ -67,7 +65,7 @@ request({
     }
     )}
     
-    , 60000);
+    , 3600000);
     
 //setInterval(coinMaker(),60000);
 
@@ -77,10 +75,10 @@ request({
 io.sockets.on('connection', function(socket) {  
     console.log('Client connected...');
         socket.emit('message', { message: 'welcome to the chat' });
-        socket.emit('message',{type: JSON.stringify(coins)});
+        socket.emit('message',{type:coins});
         setInterval(function(){
-            socket.emit('message',{type:JSON.stringify(coins)});
-        },65000);
+            socket.emit('message',{type: coins})
+        },3600000);
     socket.on('join', function(data) {
        console.log(data.my);
        socket.emit('join', {my: 'hi'});
--- bower_components/javascript/test.js
+++ bower_components/javascript/test.js
@@ -1,29 +1,74 @@
     
     /* Socket front end and dimple(d3) */
-    
-    
+    var crypt = [];
+    var cryptoCoin = []; 
      var socket = io.connect('https://test-alexaxel98.c9users.io');
         socket.on('message', function(data) {
-        console.log(data.message);
-        console.log(data.type);
+       //console.log(data.type);
+       crypt = data;
+       console.log(crypt);
         socket.emit('join', {my:'Hello World from client'});
+        
         });
     
     
     
-    
+    if(crypt.length == 0){
+      setTimeout(function(){
+        console.log(crypt.type[0]);
+        console.log(crypt.type[crypt.type.length -1]);
+      
+        console.log(crypt.type.length);
+      
     
     /* -------------------------------------------------------------*/
     // Dimple end(D3js)
     
-    var svg = dimple.newSvg("body", 800, 600);
-    var data = [
-      { "Word":"Hello", "Awesomeness":2000 },
+    for (var i = 0; i < crypt.type.length ; i++){
+        if(!isNaN(crypt.type[i].timestamp)){
+            if((crypt.type[i+1] == undefined) || (crypt.type[i].timestamp != crypt.type[i+1].timestamp)){
+        cryptoCoin.push({'Timestamp':crypt.type[i].timestamp , 'Price':crypt.type[i].ticker.price})
+                
+            }
+        }
+        console.log(cryptoCoin);
+    }
+    console.log('test');
+    
+    
+var y;
+console.log(cryptoCoin);
+var svg = dimple.newSvg("body", 1000,1000); 
+
+var chart = new dimple.chart(svg,cryptoCoin);
+    //chart.setBounds(60, 30, 510, 305)
+
+chart.addCategoryAxis("x","Timestamp"); 
+y = chart.addMeasureAxis("y","Price");
+//chart.addLogAxis("y", "Price",5);
+y.overrideMin= 600;
+chart.addSeries(null, dimple.plot.line); 
+chart.draw(); 
+
+
+    
+},500)
+
+
+
+
+
+
+    }
+   /*     var svg = dimple.newSvg("body", 800, 600);
+    var info = [
+      { "Word":"hello","Awesomeness":2000 },
       { "Word":"World", "Awesomeness":3000 }
     ];
-    var chart = new dimple.chart(svg, data);
+    var chart = new dimple.chart(svg, info);
     chart.addCategoryAxis("x", "Word");
     chart.addMeasureAxis("y", "Awesomeness");
-    chart.addSeries(null, dimple.plot.bar);
+    chart.addSeries(null, dimple.plot.line);
     chart.draw();
+    */
 
