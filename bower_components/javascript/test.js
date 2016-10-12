51b572724b62bd74cbb229d35ae60d58ac7b95ef
Luis Diaz <preston-91@hotmail.com>
2016-10-12 12:50:01 +0000

    third update

--- bower_components/javascript/test.js
+++ bower_components/javascript/test.js
@@ -1,12 +1,15 @@
     
+    var date ;
+    var chart;
     /* Socket front end and dimple(d3) */
     var crypt = [];
     var cryptoCoin = []; 
-     var socket = io.connect('https://test-alexaxel98.c9users.io');
+    var socket = io.connect('https://test-alexaxel98.c9users.io');
+    
         socket.on('message', function(data) {
        //console.log(data.type);
        crypt = data;
-       console.log(crypt);
+       //console.log(crypt);
         socket.emit('join', {my:'Hello World from client'});
         
         });
@@ -14,50 +17,82 @@
     
     
     if(crypt.length == 0){
+      
       setTimeout(function(){
-        console.log(crypt.type[0]);
-        console.log(crypt.type[crypt.type.length -1]);
-      
-        console.log(crypt.type.length);
-      
+            //console.log(crypt.type[0]);
+            //console.log(crypt.type[crypt.type.length -1]);
+           // console.log(crypt.type.length);
     
     /* -------------------------------------------------------------*/
-    // Dimple end(D3js)
+    // Dimple start(D3js)
     
-    for (var i = 0; i < crypt.type.length ; i++){
-        if(!isNaN(crypt.type[i].timestamp)){
-            if((crypt.type[i+1] == undefined) || (crypt.type[i].timestamp != crypt.type[i+1].timestamp)){
-        cryptoCoin.push({'Timestamp':crypt.type[i].timestamp , 'Price':crypt.type[i].ticker.price})
+            for (var i = 0; i < crypt.type.length ; i++){
+                if(!isNaN(crypt.type[i].timestamp)){
+                    if((crypt.type[i+1] == undefined) || (crypt.type[i].timestamp != crypt.type[i+1].timestamp)){
+                        cryptoCoin.push({'Timestamp': date = new Date(crypt.type[i].timestamp * 1000) , 'Price':crypt.type[i].ticker.price})
                 
             }
         }
-        console.log(cryptoCoin);
+        //console.log(cryptoCoin);
     }
-    console.log('test');
-    
-    
+    //console.log('test');
 var y;
-console.log(cryptoCoin);
-var svg = dimple.newSvg("body", 1000,1000); 
+var x;
 
-var chart = new dimple.chart(svg,cryptoCoin);
+
+
+    /* --------------------------------------------------------------*/
+    // supporting function for reducing ticks
+    
+    
+     // Pass in an axis object and an interval.
+ var cleanAxis = function (axis, oneInEvery) {
+     // This should have been called after draw, otherwise do nothing
+     if (axis.shapes.length > 0) {
+         // Leave the first label
+         var del = false;
+         // If there is an interval set
+         if (oneInEvery > 1) {
+             // Operate on all the axis text
+             axis.shapes.selectAll("text")
+             .each(function (d) {
+                 // Remove all but the nth label
+                 if (del % oneInEvery !== 0) {
+                     this.remove();
+                     // Find the corresponding tick line and remove
+                     axis.shapes.selectAll("line").each(function (d2) {
+                         if (d === d2) {
+                             this.remove();
+                         }
+                     });
+                 }
+                 del += 1;
+             });
+         }
+     }
+ };
+    /* --------------------------------------------------------------*/ 
+    // D3 DimpleJS continued
+
+console.log(cryptoCoin);
+var svg = dimple.newSvg(".container .jumbotron", 1000,1000); 
+
+ chart = new dimple.chart(svg,cryptoCoin);
     //chart.setBounds(60, 30, 510, 305)
 
-chart.addCategoryAxis("x","Timestamp"); 
+x=chart.addCategoryAxis("x","Timestamp"); 
 y = chart.addMeasureAxis("y","Price");
 //chart.addLogAxis("y", "Price",5);
 y.overrideMin= 600;
+
 chart.addSeries(null, dimple.plot.line); 
-chart.draw(); 
+chart.draw();
 
 
-    
+
+cleanAxis(x,5);
+
 },500)
-
-
-
-
-
 
     }
    /*     var svg = dimple.newSvg("body", 800, 600);
---
+++ bower_components/react/myapp.jsx
@@ -0,0 +1,16 @@
+var Page = React.createClass({
+    render:function(){
+
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-12" >
+                        <div className="top fontSize">
+                            <h1 className="page-h4">Bitcoin Prices </h1>
+                        </div>
+                    </div>
+                </div>
+                </div>
+                );
+    }
+});
